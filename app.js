f0013fa6e0fcb7ff86de009e42f828adbc289986
jung-han <hanjung@nhnent.com>
2018-08-17 18:39:35 +0900

    git init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    "text": "Hello World!",
+    "responseType": "ephemeral"
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
